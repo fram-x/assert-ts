f172171be32baa50cb0cbcf3ca5ab35a50b059b4
Bjørn Egil Hansen <bjorn.egil@framx.no>
2020-12-07 00:27:48 +0100

    Assert soft
    - Added `assert.soft` variant to check a condition/value and just log warning when false/null/undefined.

--- src/__tests__/assert.spec.ts
+++ src/__tests__/assert.spec.ts
@@ -1,40 +1,158 @@
-import {assert} from '../assert';
+import {assert, configureAssert} from '../assert';
+
+let logHistory: string[] = [];
+const log = (msg: string) => logHistory.push(msg);
 
 describe('assert', () => {
-  it('assert true should not fail', () => {
-    expect(() => assert(true)).not.toThrowError();
+  beforeAll(() => {
+    configureAssert({
+      errorReporter: (failureType, error, message, props) => {
+        log(`ERROR: ${failureType}, ${error}, ${message}: ${props}`);
+      },
+      warningReporter: (failureType, message, props) => {
+        log(`WARNING: ${failureType}, ${message}: ${props}`);
+      },
+    });
   });
-  it('assert false should fail', () => {
-    expect(() => assert(false)).toThrowError();
+
+  describe('hard (default)', () => {
+    it('assert true should not fail', () => {
+      expect(() => assert(true)).not.toThrowError();
+    });
+    it('assert false should fail', () => {
+      expect(() => assert(false)).toThrowError();
+    });
+    it('assert undefiined should fail', () => {
+      expect(() => assert(undefined)).toThrowError();
+    });
+    it('assert null should fail', () => {
+      expect(() => assert(null)).toThrowError();
+    });
+    it('assert an empty string should not fail', () => {
+      expect(() => assert('')).not.toThrowError();
+    });
+    it('assert a non-empty string should not fail', () => {
+      expect(() => assert('string')).not.toThrowError();
+    });
+    it('assert an empty array should not fail', () => {
+      expect(() => assert([])).not.toThrowError();
+    });
+    it('assert a non-empty array should not fail', () => {
+      expect(() => assert([5])).not.toThrowError();
+    });
+    it('assert an empty object should not fail', () => {
+      expect(() => assert({})).not.toThrowError();
+    });
+    it('assert a non-empty object should not fail', () => {
+      expect(() => assert({n: 5})).not.toThrowError();
+    });
+    it('assert 0 should not fail', () => {
+      expect(() => assert(0)).not.toThrowError();
+    });
+    it('assert 1 should not fail', () => {
+      expect(() => assert(1)).not.toThrowError();
+    });
   });
-  it('assert undefiined should fail', () => {
-    expect(() => assert(undefined)).toThrowError();
-  });
-  it('assert null should fail', () => {
-    expect(() => assert(null)).toThrowError();
-  });
-  it('assert an empty string should not fail', () => {
-    expect(() => assert('')).not.toThrowError();
-  });
-  it('assert a non-empty string should not fail', () => {
-    expect(() => assert('string')).not.toThrowError();
-  });
-  it('assert an empty array should not fail', () => {
-    expect(() => assert([])).not.toThrowError();
-  });
-  it('assert a non-empty array should not fail', () => {
-    expect(() => assert([5])).not.toThrowError();
-  });
-  it('assert an empty object should not fail', () => {
-    expect(() => assert({})).not.toThrowError();
-  });
-  it('assert a non-empty object should not fail', () => {
-    expect(() => assert({n: 5})).not.toThrowError();
-  });
-  it('assert 0 should not fail', () => {
-    expect(() => assert(0)).not.toThrowError();
-  });
-  it('assert 1 should not fail', () => {
-    expect(() => assert(1)).not.toThrowError();
+
+  describe('soft', () => {
+    beforeEach(() => {
+      logHistory = [];
+    });
+    it('assert.soft true should return true', () => {
+      const val = true;
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft true should not log', () => {
+      const val = true;
+      assert.soft(val);
+      expect(logHistory.length).toBe(0);
+    });
+    it('assert.soft false should return false', () => {
+      const val = false;
+      if (assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft false should log warning', () => {
+      const val = false;
+      assert.soft(val);
+      expect(logHistory[0]).toContain('WARNING');
+    });
+    it('assert.soft undefiined should return false', () => {
+      const val = undefined;
+      if (assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft null should return false', () => {
+      const val = null;
+      if (assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft an empty string should return true', () => {
+      const val = '';
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft a non-empty string should return true', () => {
+      const val = 'test';
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft an empty array should return true', () => {
+      const val: any[] = [];
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft a non-empty array should return true', () => {
+      const val = [5];
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft an empty object should not fail', () => {
+      const val = {};
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft a non-empty object should not fail', () => {
+      const val = {a: 'b'};
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft 0 should not fail', () => {
+      const val = 0;
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft 1 should not fail', () => {
+      const val = 1;
+      if (!assert.soft(val)) {
+        fail();
+      }
+    });
+    it('assert.soft T? should constrain type to T when proper value', () => {
+      type P = {
+        name: string;
+      };
+      const ps: (P | undefined)[] = [{name: 'n'}, undefined];
+
+      ps.forEach((p?: P) => {
+        if (assert.soft(p)) {
+          expect(p.name).toBe('n');
+        } else {
+          expect(p).toBe(undefined);
+        }
+      });
+    });
   });
 });
--- src/assert.ts
+++ src/assert.ts
@@ -3,7 +3,17 @@ enum FailureType {
   NoValue = 'NoValue',
 }
 
+enum Strictness {
+  Strict = 'Strict',
+  Soft = 'Soft',
+}
+
 type Formatter = (
+  /**
+   * Strict: assert(...) failed
+   * Soft: assert.soft(...) failed
+   */
+  strictness: Strictness,
   failureType: FailureType,
   message?: string,
   props?: object,
@@ -22,27 +32,45 @@ type ErrorReporter = (
   props?: object,
 ) => void;
 
+type WarningReporter = (
+  failureType: FailureType,
+  message?: string,
+  props?: object,
+) => void;
+
 export type AssertConfiguration = {
   formatter?: Formatter;
   errorCreator?: ErrorCreator;
   errorReporter?: ErrorReporter;
+  warningReporter?: WarningReporter;
 };
 
 type RequiredConfiguration = {
   formatter: Formatter;
   errorCreator: ErrorCreator;
   errorReporter?: ErrorReporter;
+  warningReporter?: WarningReporter;
 };
 
-const errorMessageFormatter: Formatter = (
+const messageFormatter: Formatter = (
+  strictness: Strictness,
   failureType: FailureType,
   message?: string,
   props?: object,
 ): string => {
+  const typeMap = {
+    [Strictness.Strict]: {
+      [FailureType.Condition]: 'Assert condition failed',
+      [FailureType.NoValue]: 'Assert value not undefined/null failed',
+    },
+    [Strictness.Soft]: {
+      [FailureType.Condition]: 'Check condition failed',
+      [FailureType.NoValue]: 'Check value not undefined/null failed',
+    },
+  };
+
   const msg =
-    (failureType === FailureType.Condition
-      ? 'Assert condition failed'
-      : 'Assert value not undefined/null failed') +
+    typeMap[strictness][failureType] +
     (message ? `: ${message}` : '') +
     (props ? `: ${JSON.stringify(props)}` : '');
 
@@ -51,12 +79,12 @@ const errorMessageFormatter: Formatter = (
 
 const errorCreatorFactory = (formatter: Formatter): ErrorCreator => {
   return (failureType: FailureType, message?: string, props?: object) =>
-    new Error(formatter(failureType, message, props));
+    new Error(formatter(Strictness.Strict, failureType, message, props));
 };
 
 const defaultConfiguration: RequiredConfiguration = {
-  formatter: errorMessageFormatter,
-  errorCreator: errorCreatorFactory(errorMessageFormatter),
+  formatter: messageFormatter,
+  errorCreator: errorCreatorFactory(messageFormatter),
 };
 
 let configuration = defaultConfiguration;
@@ -84,37 +112,72 @@ export function testResetConfiguration() {
   configuration = defaultConfiguration;
 }
 
-/**
- * Verify that a condition is satisfied.
- * @param condition Condition to be true
- * @param message Error message
- * @param props Any props relevant.
- * @throws Throws exception if condition is false.
- */
-export function assert(
-  condition: boolean,
-  message?: string,
-  props?: object | (() => object),
-): asserts condition;
+interface HardAssert {
+  /**
+   * Verify that a condition is satisfied.
+   * @param condition Condition to be true
+   * @param message Error message
+   * @param props Any props relevant.
+   * @throws Throws exception if condition is false.
+   */
+  (
+    condition: boolean,
+    message?: string,
+    props?: object | (() => object),
+  ): asserts condition;
 
-/**
- * Verify that an optional value actually has a proper value in this context, i.e. not null or undefined.
- * @param value Value to be verified
- * @param message Error message
- * @param props If message is a string id, format any matching key values into message. Props are also reported to dev team.
- * @throws Throws exception if value is null or undefined
- */
-export function assert<T>(
-  value: T | undefined | null,
-  message?: string,
-  props?: object | (() => object),
-): T;
+  /**
+   * Verify that an optional value actually has a proper value in this context, i.e. not null or undefined.
+   * @param value Value to be verified
+   * @param message Error message
+   * @param props If message is a string id, format any matching key values into message. Props are also reported to dev team.
+   * @throws Throws exception if value is null or undefined
+   */
+  <T>(
+    value: T | undefined | null,
+    message?: string,
+    props?: object | (() => object),
+  ): T;
+}
 
-export function assert<T>(
+interface SoftAssert {
+  /**
+   * Test if a condition is satisfied. If false, a warning will be reported
+   * @param condition Condition to be true
+   * @param message Warning message
+   * @param props Any props relevant.
+   * @returns condition
+   */
+  (
+    condition: boolean,
+    message?: string,
+    props?: object | (() => object),
+  ): condition is true;
+
+  /**
+   * Test if an optional value actually is a proper value, i.e. not null or undefined. If value is not proper, a warning
+   * will be reported
+   * @param value Value to be tested
+   * @param message Warning message
+   * @param props If message is a string id, format any matching key values into message. Props are also reported to dev team.
+   * @throws Throws exception if value is null or undefined
+   */
+  <T>(
+    value: T | undefined | null,
+    message?: string,
+    props?: object | (() => object),
+  ): value is T;
+}
+
+interface Assert extends HardAssert {
+  soft: SoftAssert;
+}
+
+const hardAssert: HardAssert = <T>(
   conditionOrValue: T | boolean | undefined | null,
   message?: string,
   props?: object | (() => object),
-): void | T {
+): void | T => {
   if (typeof conditionOrValue === 'boolean') {
     if (!conditionOrValue) {
       const properties = typeof props === 'function' ? props() : props;
@@ -159,4 +222,59 @@ export function assert<T>(
   }
 
   return conditionOrValue;
-}
+};
+
+const softAssert: SoftAssert = <T>(
+  conditionOrValue: T | boolean | undefined | null,
+  message?: string,
+  props?: object | (() => object),
+): conditionOrValue is T | true => {
+  if (typeof conditionOrValue === 'boolean') {
+    if (!conditionOrValue) {
+      const properties = typeof props === 'function' ? props() : props;
+
+      if (configuration.warningReporter) {
+        configuration.warningReporter(
+          FailureType.Condition,
+          message,
+          properties,
+        );
+      } else {
+        const formattedMessage = configuration.formatter(
+          Strictness.Soft,
+          FailureType.Condition,
+          message,
+          properties,
+        );
+        console.warn(formattedMessage);
+      }
+    }
+
+    return conditionOrValue;
+  }
+
+  if (conditionOrValue === undefined || conditionOrValue === null) {
+    const properties = typeof props === 'function' ? props() : props;
+
+    if (configuration.warningReporter) {
+      configuration.warningReporter(FailureType.NoValue, message, properties);
+    } else {
+      const formattedMessage = configuration.formatter(
+        Strictness.Soft,
+        FailureType.NoValue,
+        message,
+        properties,
+      );
+      console.warn(formattedMessage);
+    }
+
+    return false;
+  }
+
+  return true;
+};
+
+const _assert = hardAssert as Assert;
+_assert.soft = softAssert;
+
+export const assert = _assert;
